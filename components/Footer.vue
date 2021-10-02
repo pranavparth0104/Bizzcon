44ffc34ec37090147631ec80307fdfc0d44c2a4e
Pranav <pranav.parth01@gmail.com>
2021-10-02 19:46:36 +0530

    edit6

--- components/Footer.vue
+++ components/Footer.vue
@@ -14,7 +14,6 @@
           <a href="/about_us/whybizzcon"><p><a  style="color: #ffff" href="/about_us/whybizzcon">ABOUT US</a></p></a>
           <a href="/jobs/post"><p><a style="color: #ffff" href="/jobs/post">JOBS</a></p></a>
           <a href="/industries"><p><a style="color: #ffff" href="/industries">INDUSTRY</a></p></a>
-          <a href="/contact_us"><p><a style="color: #ffff" href="/contact_us">CONTACT US</a></p></a>
         </mdb-col>
         <hr class="w-100 clearfix d-md-none" />
         <mdb-col md="3" lg="2" xl="2" class="mx-auto mt-3">
@@ -28,8 +27,8 @@
           <h6 style="color: #ffff" class="text-uppercase mb-4 font-weight-bold">Contact Us</h6>
           <p style="color: #ffff"><i class="fas fa-home mr-3"></i> BENGALURU</p>
           <p style="color: #ffff"><i class="fas fa-envelope mr-3"></i>BIzzcon Solutions Pvt. Ltd.</p>
-                    <p style="color: #ffff"><i class="fas fa-envelope mr-3"></i>OMBR Layout </p>
-          <p style="color: #ffff"><i class="fas fa-envelope mr-3"></i> Kalyan Nagar, Bengaluru 560043
+                    <p style="color: #ffff"><i class="fas fa-envelope mr-3"></i>OMBR Layout, Kalyan Nagar </p>
+<p style="color: #ffff"><i class="fas fa-envelope mr-3"></i>Bengaluru 560043
 Karnataka</p>
 
           <!-- <p style="color: #ffff"><i class="fas fa-phone mr-3"></i> +91 11 4934 3900</p>
@@ -39,7 +38,7 @@ Karnataka</p>
       <hr />
       <mdb-row class="d-flex align-items-center">
         <mdb-col md="8" lg="8">
-          <p style="color: #ffff" class="text-center text-md-left grey-text">&copy; 2021 Copyright: Business consulting</p>
+          <p style="color: #ffff" class="text-center text-md-left grey-text">&copy; 2021 Copyright: Business Solutions</p>
         </mdb-col>
         <mdb-col md="4" lg="4" class="ml-lg-0">
           <div class="text-center text-md-right">
